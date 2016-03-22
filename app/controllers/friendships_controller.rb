08b1f30f9cfbde821de2dbc554a82c74266cc35d
sco-bo <scottebobbitt@gmail.com>
2016-03-22 13:38:55 -0400

    tweak friendships_controller and add Bootstrap

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,23 +1,40 @@
 class FriendshipsController < ApplicationController
-  # def index
-  #   @user = User.find(params[:id])
-  #   @friendship = @user.friendships 
-  # end
-
-  # def update
-  #   @user = User.find(params[:id])
-  #   @friendship = @user.friendships.find(params[:id])
-  #   # @friendship.accept()
-  # end
-
-  # def destroy
-  # end
+  before_action :find_friend
 
   def accept
-    @friend = User.find(params[:friend_id])
     if current_user.requested_friends.include?(@friend)
       Friendship.accept(current_user, @friend)
-      flash[:notice] = "Friendship accepted"
+      flash[:notice] = "Friendship with #{@friend.email} accepted"
+    else
+      flash[:notice] = "No Friendship request"
+    end
+    redirect_to root_url
+  end
+
+  def decline
+    if current_user.requested_friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Friendship with #{@friend.email} declined"
+    else
+      flash[:notice] = "No Friendship request"
+    end
+    redirect_to root_url
+  end
+
+  def delete
+    if current_user.friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Successfully unfriended #{@friend.email}"
+    else
+      flash[:notice] = "No Friendship request"
+    end
+    redirect_to root_url
+  end
+
+  def cancel
+    if current_user.pending_friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Friend request to #{@friend.email} retracted"
     else
       flash[:notice] = "No Friendship request"
     end
@@ -25,8 +42,13 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friend = User.find(params[:friend_id])
     Friendship.request(current_user, @friend)
+    flash[:notice] = "Friend request sent"
     redirect_to users_index_path
   end
+
+  private
+    def find_friend
+      @friend = User.find(params[:friend_id])
+    end
 end
