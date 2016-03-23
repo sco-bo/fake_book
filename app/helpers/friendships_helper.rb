6cd3837b77435bd319bbcf42fde27bb2720c18fd
sco-bo <scottebobbitt@gmail.com>
2016-03-23 16:06:45 -0400

    add notifications dropdown to navbar with friend requests

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,6 @@
 module FriendshipsHelper
+
+  def friend_request? 
+    !current_user.requested_friends.nil?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-password = "password"
+User.delete_all
+Friendship.delete_all
+
+User.create!(
+  name: "Scott",
+  email: "scott@gmail.com",
+  password: "password"
+  )
+
+User.create!(
+  name: "Dave",
+  email: "dave@gmail.com",
+  password: "password"
+  )
+
+User.create!(
+  name: "Zach",
+  email: "Zach@gmail.com",
+  password: "password"
+  )
 
 50.times do |n|
   user = User.new(
+    name: Faker::Name.name,
     email: Faker::Internet.email,
     password: "password"
     )
