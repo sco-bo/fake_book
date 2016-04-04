84ff75d506bb4afc84120a0c8e303ff3691752ff
sco-bo <scottebobbitt@gmail.com>
2016-04-04 13:41:29 -0400

    add post/comment/like faker seed data and fix time_ago_in_words error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 User.delete_all
 Friendship.delete_all
+Post.delete_all
+Comment.delete_all
+Like.delete_all
 
 User.create!(
   name: "Scott",
@@ -19,7 +22,7 @@ User.create!(
   password: "password"
   )
 
-50.times do |n|
+100.times do |n|
   user = User.new(
     name: Faker::Name.name,
     email: Faker::Internet.email,
@@ -27,3 +30,52 @@ User.create!(
     )
   user.save
 end
+
+# Posts
+users = User.order(:created_at)
+10.times do 
+  users.each do |user|
+    body = Faker::Hipster.sentence
+    user.posts.create!(body: body)
+  end
+end
+
+# Friendships
+users = User.all
+pending = users[0..30]
+requested = users[31..60]
+accepted = users[31..45]
+final_requested = users[61..90]
+
+pending.each do |n|
+  requested.each do |i|
+    Friendship.request(n, i)
+  end
+end
+
+accepted.each do |n|
+  pending.each do |i|
+    Friendship.accept(n, i)
+  end
+end
+
+final_requested.each do |n|
+  pending.each do |i|
+    Friendship.request(n, i)
+  end
+end
+
+#Likes and comments
+users.each do |user|
+  posts = Post.where(user: user.friends).order("RANDOM()").take(6)
+  posts.each do |post|
+    comment = Faker::StarWars.quote
+    post.likes.create!(user_id: user.id)
+    post.comments.create!(user_id: user.id, post_id: post.id, comment: comment)
+  end
+end
+
+
+
+
+
