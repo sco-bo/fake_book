8e268cb7827692d60d80947c157684c043ef7068
sco-bo <scottebobbitt@gmail.com>
2016-04-04 15:30:43 -0400

    style alert messages

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,14 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty? 
+
+    messages = resource.errors.full_messages.map {|message| content_tag(:li, message) }.join
+    html = <<-HTML 
+    <div class="alert alert-error alert-danger"> <button type="button" class="close" data-dismiss="alert">x</button>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
